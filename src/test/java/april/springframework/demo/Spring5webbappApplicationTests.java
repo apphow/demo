7e0e5b976427756b7c6bc0680b89a8d48c0f8fdb
April Howard <aprilhoward@Aprils-MBP.fios-router.home>
2020-08-22 15:57:09 -0400

    Initial commit

---
+++ src/test/java/april/springframework/demo/Spring5webbappApplicationTests.java
@@ -0,0 +1,13 @@
+package april.springframework.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
